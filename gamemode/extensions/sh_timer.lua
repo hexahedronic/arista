2486b867c3a4b06bab3ca0c0a2ab65a8e8eef895
CaveeJohnson <caveejohnson@hotmail.com>
2016-03-02 00:03:39 +0000

    Conditional Timer

--- gamemode/extensions/sh_timer.lua
+++ gamemode/extensions/sh_timer.lua
@@ -1,3 +1,23 @@
 AddCSLuaFile()
 
 arista.timer = {}
+
+function arista.timer.conditional(name, time, conditional, success, failure, ...)
+	local args = {...}
+
+	-- todo: VarArgs are finicky, replace this if possible please.
+	local suc = function()
+		success(unpack(args))
+	end
+	timer.Create(name, time + 1, 1, suc)
+
+	local tick = function()
+		if conditional(unpack(args)) then
+			failure(unpack(args))
+
+			timer.Destroy(name)
+			timer.Destroy(name .. "_tick")
+		end
+	end
+	timer.Create(strName .. "_tick", 1, time, tick)
+end

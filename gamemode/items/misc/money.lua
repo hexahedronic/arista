3c0f8f6736692d87c1ad0a52ebd4e3e469cadca9
CaveeJohnson <caveejohnson@hotmail.com>
2016-03-16 17:33:35 +0000

    Money for containers

---
+++ gamemode/items/misc/money.lua
@@ -0,0 +1,29 @@
+ITEM.name					= "Note"
+ITEM.plural				= "Notes"
+ITEM.size					= 0
+ITEM.cost					= 1
+ITEM.model				= "models/props/cs_assault/money.mdl"
+ITEM.batch				= 1000
+ITEM.store				= false
+ITEM.description	= "A wad of money."
+
+local function updatefunc(ply)
+	if not ply:IsValid() then return end
+
+	ply:giveMoney(ply._temporaryMoneyUpdate)
+	ply._temporaryMoneyUpdate	= nil
+end
+
+function ITEM:onUpdate(ply, amount)
+	if amount > 0 then
+		if ply._temporaryMoneyUpdate then
+			ply._temporaryMoneyUpdate	= ply._temporaryMoneyUpdate + amount
+		else
+			ply._temporaryMoneyUpdate	= amount
+
+			timer.Simple(0.1, function() updatefunc(ply) end)
+		end
+
+		return true -- Never put this in a player's inventory
+	end
+end

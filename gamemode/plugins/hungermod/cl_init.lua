0647845a42641fbc1b89c3d50fff84818ae61766
CaveeJohnson <caveejohnson@hotmail.com>
2016-03-26 09:47:33 +0000

    style fix, drain hook, slight consistancy improvement

--- gamemode/plugins/hungermod/cl_init.lua
+++ gamemode/plugins/hungermod/cl_init.lua
@@ -4,14 +4,10 @@ arista.lang:Add("AL_HUD_HUNGER", {
 
 -- Called when the bottom bars should be drawn.
 function PLUGIN:DrawBottomBars(bar)
-
 	local hunger = arista.lp:getAristaInt("hunger") or 100
 
 	-- Check if the hunger is smaller than 100.
 	if hunger < 100 then
-	
 		GAMEMODE:DrawBar("Default", bar.x, bar.y, bar.width, bar.height, color_darkgreen, arista.lang:Get"AL_HUD_HUNGER" .. hunger .. "%", 100, hunger, bar)
-		
 	end
-	
 end
--- gamemode/plugins/hungermod/sv_init.lua
+++ gamemode/plugins/hungermod/sv_init.lua
@@ -1,49 +1,40 @@
 function PLUGIN:PostPlayerSpawn(ply, lightSpawn, changeTeam)
-
 	if not lightSpawn then
-	
 		ply:networkAristaVar("hunger", 100)
-		-- Make the hunger save and load from previous session?
-		
+		-- todo: Make the hunger save and load from previous session?
 	end
-	
 end
 
 local LastDelay = CurTime()
-
 function PLUGIN:PlayerTenthSecond(ply)
-
 	if LastDelay > CurTime() then return end
 	LastDelay = CurTime() + arista.config.plugins.hungerInterval
 
+	local drain = arista.config.plugins.hungerDrain
+	drain = hook.Run("HungerAdjustDrain") or drain
+
 	local hunger = ply:getAristaVar("hunger") or 100
 	hunger = math.Clamp(hunger - arista.config.plugins.hungerDrain, 0, 100)
+
 	ply:setAristaVar("hunger", hunger)
-	
 end
 
 function PLUGIN:StaminaAdjustPlayerSpeed(ply, run, walk)
+	local hungry = (ply:getAristaVar("hunger") or 100) <= 10
 
-	local hunger = ply:getAristaVar("hunger") or 100
-	
 	if hunger <= 10 then
-	
 		run = walk
+
 		return run, walk
-		
 	end
-	
 end
 
 function PLUGIN:StaminaAdjustDrain(ply, amt)
-
 	local hungry = (ply:getAristaVar("hunger") or 100) <= 10
-	
-	if hungry then 
-	
-		amt = amt * 1.5 
-		return amt 
-		
+
+	if hungry then
+		amt = amt * 1.5
+
+		return amt
 	end
-	
 end

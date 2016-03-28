7032826afa8118638750687867f4d75f26d9e511
CaveeJohnson <caveejohnson@hotmail.com>
2016-03-28 12:02:11 +0100

    More hooks, -- todo: add to framework.txt

---
+++ gamemode/hooks/sv_entity.lua
@@ -0,0 +1,23 @@
+function GM:EntityMadeOwnable(entity)
+end
+
+function GM:EntityUnMadeOwnable(entity)
+end
+
+function GM:EntityAccessChangedPlayer(entity, player, bool)
+end
+
+function GM:EntityMasterSet(entity, master)
+end
+
+function GM:EntityGiveSlave(entity, slave)
+end
+
+function GM:EntityTakeSlave(entity, slave)
+end
+
+function GM:EntityOwnerSet(entity, kind, owner)
+end
+
+function GM:EntityNameSet(entity, name)
+end

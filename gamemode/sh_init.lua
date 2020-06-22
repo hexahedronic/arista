d64f4e0f483314eac2524dbfcdf33665768bba6e
Conn Beckitt-Marshall <conn@jafico.co.uk>
2020-06-22 13:55:06 +0100

    Money formatting

--- gamemode/sh_init.lua
+++ gamemode/sh_init.lua
@@ -348,3 +348,36 @@ function GM:CanTool(ply, trace, tool)
 	-- Call the base class function.
 	return self.BaseClass:CanTool(ply, trace, tool)
 end
+
+-- Number Formatting
+
+function formatNum(amount, decimal, prefix, neg_prefix)
+	local formatted, famount, remain
+	decimal = decimal or 2 -- default 2 decimal places
+	neg_prefix = neg_prefix or "-" -- default negative sign
+	famount = math.abs(round(amount, decimal))
+	famount = math.floor(famount)
+	remain = round(math.abs(amount) - famount, decimal)
+	-- comma to separate the thousands
+	formatted = comma_value(famount)
+
+	-- attach the decimal portion
+	if (decimal > 0) then
+		remain = string.sub(tostring(remain), 3)
+		formatted = formatted .. "." .. remain .. string.rep("0", decimal - string.len(remain))	
+	end
+
+	-- attach prefix string e.g '$' 
+	formatted = (prefix or "") .. formatted
+
+	-- if value is negative then format accordingly
+	if (amount < 0) then
+		if (neg_prefix == "()") then
+			formatted = "(" .. formatted .. ")"
+		else
+			formatted = neg_prefix .. formatted
+		end
+	end
+
+	return formatted
+end

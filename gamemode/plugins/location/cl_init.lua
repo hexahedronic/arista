70cd5b4d15949d6051680f06158b5c53bf5dd85c
Conn Beckitt-Marshall <conn@jafico.co.uk>
2020-06-25 20:26:52 +0100

    Update cl_init.lua

--- gamemode/plugins/location/cl_init.lua
+++ gamemode/plugins/location/cl_init.lua
@@ -9,16 +9,59 @@ net.Receive( "LocationHook", function( len )
 end)
 
 
+surface.CreateFont( "CopperplateNow", {
+	font = "Copperplate Bold", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 22,
+	weight = 300,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
+
+surface.CreateFont( "Copperplate", {
+	font = "Copperplate Bold", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 35,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
+
 local function locationDisplay(location)
-    local ply = LocalPlayer()
-    surface.SetFont("Trebuchet24")
-    surface.SetMaterial(HUDMaterial)
+	surface.SetFont("CopperplateNow")
+    local widthtop = surface.GetTextSize("Now Entering:")
+    local xt = ScrW()/2 - widthtop/2
+	surface.SetFont("Copperplate")
     local width = surface.GetTextSize(location)
-    local height = 300
-    local x = ScrW() - (width/2)
-    local y = ScrH() * 0.3
-    surface.DrawTexturedRect(x, y, width, height)
-    surface.SetTextPos((ScrW() / 2) - (width / 2), y * 1.1)
+    local height = 160
+    local x = ScrW()/2 - width/2
+    local y = ScrH() * 0.1
+    surface.SetMaterial(HUDMaterial)
+    surface.SetDrawColor( 255, 255, 255 )
+    surface.DrawTexturedRect(x - 50, y, width + 100, height)
+    surface.SetFont("CopperplateNow")
+    surface.SetTextPos(xt , y * 1.2)
+    surface.DrawText("Now Entering:")
+    surface.SetFont("Copperplate")
+    surface.SetTextPos(x , y * 1.4)
     surface.DrawText(location)
 end
 

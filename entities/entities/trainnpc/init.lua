e1a5cfe8443b65e56c20a16dc2f587821c0c3d59
ChaiLatte123 <ohmygodnooneshasthis@gmail.com>
2020-07-02 22:39:50 +0100

    Update init.lua

--- entities/entities/trainnpc/init.lua
+++ entities/entities/trainnpc/init.lua
@@ -32,7 +32,7 @@ net.Receive("Train_Journey", function(len, ply)
 	ply:SetPos(Vector(7273.56, -7210.74, 208.03));
 end);
 
-function ENT:PhysgunPickup(ply)
+function PhysgunPickup(ply)
 	if (ply:IsAdmin()) then return true;
 	else return false end;
 end

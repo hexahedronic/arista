b665754414767b877f70b0aaf37b62797d679894
ChaiLatte123 <ohmygodnooneshasthis@gmail.com>
2020-07-05 21:03:29 +0100

    Update init.lua

--- entities/entities/arista_trainnpc_fillcum/init.lua
+++ entities/entities/arista_trainnpc_fillcum/init.lua
@@ -30,8 +30,8 @@ end
 net.Receive("Train_Journey", function(len, ply)
 	if(ply:getMoney() >= 0.25) then
 		ply:giveMoney(-0.25);
-		ply:ChatPrint("You ride the train to Fillcum Ridge...");
-		ply:SetPos(Vector(7273.56, -7210.74, 208.03));
+		ply:ChatPrint("You ride the train to Greenwood Ridge...");
+		ply:SetPos(Vector(-12125.03, -5315.12, 112.03));
 	else
 		ply:ChatPrint("You have insufficient funds!")
 	end
